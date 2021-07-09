e966d4c444c43d9baf8a26f6eb95d66617818f81
yongrae <kyr9389@naver.com>
2021-07-09 19:28:12 +0900

    UPDATE generated Thumbnail finally serve
    Signed-off-by: yongrae <kyr9389@naver.com>

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var imageRouter = require('./routes/image');
@@ -19,6 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cors());
 app.use('/', indexRouter);
 app.use('/img', imageRouter);
 
--- routes/image.js
+++ routes/image.js
@@ -2,28 +2,19 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
+var puppeteer = require('puppeteer');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('잘못된 요청입니다.');
 });
 
-router.post('/', function(req, res, next) {
+router.post('/', async function(req, res, next) {
   const image_url = req.body.image_url;
   const title_main = req.body.title_main;
   const title_sub = req.body.title_sub;
   const bg_color = req.body.bg_color;
   const font_color = req.body.font_color;
-/*
-  html2canvas(document.getElementById("container"), {
-    onrendered: function (canvas) {
-      let a = document.createElement('a');
-      a.href = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
-      a.download = "out.png";
-      a.click();
-    }
-  });
-*/
 
   res.render('img', {
     image_url: image_url,
@@ -32,6 +23,49 @@ router.post('/', function(req, res, next) {
     bg_color: bg_color,
     font_color: font_color
   });
+});
+
+router.post('/req', async function(req, res, next) {
+  const image_url = req.body.image_url;
+  const title_main = req.body.title_main;
+  const title_sub = req.body.title_sub;
+  const bg_color = req.body.bg_color;
+  const font_color = req.body.font_color;
+
+  const browser = await puppeteer.launch({
+    args: ["--enable-features=NetworkService", "--no-sandbox"],
+    ignoreHTTPSErrors: true
+  });
+  const page = await browser.newPage()
+
+  // Post로 요청을 보내기 위해 요청을 Intercept 합니다.
+  await page.setRequestInterception(true);
+
+  await page.once("request", interceptedRequest => {
+    interceptedRequest.continue({
+      method: "POST",
+      postData: 'image_url=' + image_url + '&title_main=' + title_main + '&title_sub=' + title_sub + '&bg_color=' + bg_color + '&font_color' + font_color,
+      headers: {
+        ...interceptedRequest.headers(),
+        "Content-Type": "application/x-www-form-urlencoded"
+      }
+    });
+
+    // 다른 요청을 처리하기 위해 Intercept를 해제합니다.
+    page.setRequestInterception(false);
+  });
+
+  await page.goto('http://localhost:3000/img')
+
+  const fileName = "capture-" + new Date().toISOString().substr(0, 10) + "-" + Math.floor(Math.random()*10000+10000) + ".png";
+
+  const element = await page.$('#container');
+  await element.screenshot({
+    path: fileName
+  })
+
+  await browser.close()
+  await res.sendFile(fileName, {root : '.'});
 });
 
 module.exports = router;
