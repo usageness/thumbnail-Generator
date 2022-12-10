e5f39c7fde21e5e01674914913ee810cc0a0260b
YongRae_Kim (Usage) <kyr9389@naver.com>
2022-12-10 23:32:26 +0900

    feat: 환경 설정에서 저장된 데이터 지우기 추가

--- src/components/OptionModal/index.tsx
+++ src/components/OptionModal/index.tsx
@@ -3,6 +3,7 @@ import Modal from 'components/Modal';
 import { ModalContext } from 'stores/modalContext';
 import { AppStateContext } from 'stores/appStateContext';
 import styles from './index.scss';
+import { localStorageKey } from 'constant/constant';
 
 function OptionModal() {
   const modalObject = useContext(ModalContext);
@@ -13,19 +14,30 @@ function OptionModal() {
   const { modalFlag } = modalObject;
   const { debugMode, toggleDebugMode } = appStateObject;
 
+  const removeLocalData = () => {
+    localStorage.removeItem(localStorageKey.latestConfiguration);
+    alert('데이터를 성공적으로 삭제했습니다');
+  };
+
   return (
     <Modal modalState={modalFlag === 'option'}>
       <h3>환경 설정</h3>
-      <div className={styles.singleInputContainer}>
-        <label htmlFor="debug_mode">디버깅 옵션</label>
-        <input
-          id="debug_mode"
-          type="checkbox"
-          name="debug_mode"
-          value="debug_mode"
-          checked={debugMode}
-          onChange={toggleDebugMode}
-        />
+      <div className={styles.menuContainer}>
+        <div className={styles.singleLineContainer}>
+          <label htmlFor="debug_mode">디버깅 옵션</label>
+          <input
+            id="debug_mode"
+            type="checkbox"
+            name="debug_mode"
+            value="debug_mode"
+            checked={debugMode}
+            onChange={toggleDebugMode}
+          />
+        </div>
+        <div className={styles.singleLineContainer}>
+          <p>저장된 데이터 지우기</p>
+          <button onClick={removeLocalData}>삭제하기</button>
+        </div>
       </div>
     </Modal>
   );
